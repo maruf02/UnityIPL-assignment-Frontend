3336b83e2ea473d613af03fe0c7a85bec45e42de
maruf02 <rashiduzzaman99@gmail.com>
2023-12-11 00:50:27 +0600

    Homepage simple design

--- src/Components/Homepage/Homepage.jsx
+++ src/Components/Homepage/Homepage.jsx
@@ -3,7 +3,24 @@ import React from "react";
 const Homepage = () => {
   return (
     <div>
-      <h2>this is homepage</h2>
+      {/* for search section */}
+      <div className="flex justify-center py-10">
+        <input
+          type="text"
+          placeholder="Type here"
+          className="input input-bordered input-info w-full max-w-xs bg-transparent"
+        />
+        <button className="btn btn-primary text-black text-lg mx-2">
+          Search
+        </button>
+      </div>
+      {/* for search result section */}
+      <div>
+        <h2 className="text-2xl underline font-bold text-center">
+          Search Result:
+        </h2>
+        <section className="container mx-auto text-center  w-3/4  h-[1000px]"></section>
+      </div>
     </div>
   );
 };
